2c3128de013ca579c8af7653d3758045b597c0be
Parth-sys <9156parth@gmail.com>
2022-01-06 16:35:10 +0530

    env

--- connection.js
+++ connection.js
@@ -2,7 +2,8 @@ const mongoose=require("mongoose");
 
 exports.connect=()=>{
     try{
-        mongoose.connect('mongodb+srv://Parth:Parth123@cluster0.cxpc1.mongodb.net/studentMentor',{useNewUrlParser:true,useUnifiedTopology:true})
+        const url=process.env.url;
+        mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true})
         console.log("db connected")
     }catch(err){
         console.log(err);
